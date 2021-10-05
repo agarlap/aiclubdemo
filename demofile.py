18574e78fa39f1f77d6b080c4ff8f573c3fd0c80
agarlap <agarlap@g.clemson.edu>
2021-10-05 01:52:13 -0400

    Added turns and a position counter

--- demofile.py
+++ demofile.py
@@ -1,7 +1,13 @@
 
 import random
 
-dice1 = random.randrange(1, 7)
+position = 0
 
-print("Dice rolled:")
-print(dice1)
+for turn in range(1, 6):
+    print("Turn #", turn,":")
+    dice1 = random.randrange(1, 7)
+    print("Dice rolled: ", dice1)
+    position = position + dice1
+    print("Position: ", position)
+
+print("OVERALL POSITION: ", position)
